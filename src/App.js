6fbfb713d706aa6eacfc1ee4d19d4461ab82f0c5
Dmitrii <dimana1507@gmail.com>
2021-08-09 10:37:13 +0300

    update render DOM using css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { PhotosProvider } from "./contexts";
 import { Gallery } from "./components";
+
+console.log('=> document', document.body.style); // eslint-disable-line no-console
 const App = () => {
   return (
     <PhotosProvider>
